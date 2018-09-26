c0dad2c0b333bfc496b0c9cb3529207439fd1c12
Tanvi Deshpande <36173090+TanviDeshpande95@users.noreply.github.com>
2018-09-26 16:07:34 -0700

    Update copy_config.sh
    Change path

--- ose/files/copy_config.sh
+++ ose/files/copy_config.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat /home/openshift/ose/files/contrail_config.txt >> /home/openshift-ansible/inventory/ose-install
+cat /home/custom-openshift/ose/files/contrail_config.txt >> /home/openshift-ansible/inventory/ose-install
