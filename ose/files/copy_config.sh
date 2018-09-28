53c7f0230debc102f09648397edb6374cc311012
Tanvi Deshpande <36173090+TanviDeshpande95@users.noreply.github.com>
2018-09-28 16:39:10 -0700

    Update copy_config.sh
    Appending contrail config in middle of the file instead of towards the end

--- ose/files/copy_config.sh
+++ ose/files/copy_config.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat /home/custom-openshift/ose/files/contrail_config.txt >> /home/openshift-ansible/inventory/ose-install
+sed -i -e '/nested_mode_contrail = false/r /home/custom-openshift/ose/files/contrail-config.txt' /home/openshift-ansible/inventory
