70087f5ae6c908a7ac0affadea8964e2f0c60d5a
Tanvi Deshpande <36173090+TanviDeshpande95@users.noreply.github.com>
2018-09-28 16:45:52 -0700

    Update copy_config.sh

--- ose/files/copy_config.sh
+++ ose/files/copy_config.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sed -i -e '/nested_mode_contrail = false/r /home/custom-openshift/ose/files/contrail-config.txt' /home/openshift-ansible/inventory
+sed -i -e '/nested_mode_contrail = false/r /home/custom-openshift/ose/files/contrail_config.txt' /home/openshift-ansible/inventory
