b086cd9c66536a1e08d768e1949c69cb99b74e11
Vinay Rao <vinarao@github.com>
2018-10-01 18:05:33 +0000

    fix the append_script

--- ose/files/append_nodes.sh
+++ ose/files/append_nodes.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
-cat /usr/share/ansible/custom-openshift/ose/files/append.txt >> /usr/share/ansible/openshift-ansible/playbooks/openshift-node/private/additional_config.yml
+dest=$1
+echo $dest
+cat $PWD/append.txt >> $dest/playbooks/openshift-node/private/additional_config.yml
 
 
