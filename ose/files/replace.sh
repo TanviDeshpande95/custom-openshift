b6a42f991e8fd8547e9b2ebf6357a56decbb01bd
Tanvi Deshpande <36173090+TanviDeshpande95@users.noreply.github.com>
2018-10-01 14:59:28 -0700

    Update replace.sh
    Fixed directories

--- ose/files/replace.sh
+++ ose/files/replace.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
-sed -i '/openshift_use_calico/a \ \ - role: contrail_master \n \ \ \ when: openshift_use_contrail | default(false) | bool' /usr/share/ansible/openshift-ansible/playbooks/openshift-master/private/config.yml
+#!/bin/bash
+dst=$1
+sed -i '/openshift_use_calico/a \ \ - role: contrail_master \n \ \ \ when: openshift_use_contrail | default(false) | bool' $dst/playbooks/openshift-master/private/config.yml
